72f7012762714586da0e6b6268b7bc4091176ec7
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-07-01 22:18:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190701162828) do
+ActiveRecord::Schema.define(version: 20190701192833) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "song"
+    t.string   "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
